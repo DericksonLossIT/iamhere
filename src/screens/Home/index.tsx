ee3a7fde63462760ee6d3a92a0c2be5c936f84f6
DericksonLossIT <dericksonlossit@gmail.com>
2022-10-31 14:32:11 -0300

    creates text input

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View, TextInput } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Segunda, 31 de Outubro de 2022</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  );
+}
