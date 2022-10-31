d5a29ed2f755b4657b97f2fe83b24c6c32d3c353
DericksonLossIT <dericksonlossit@gmail.com>
2022-10-31 14:40:13 -0300

    styles input and button in a wrapper

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,28 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Clique no botão de add");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Segunda, 31 de Outubro de 2022</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
