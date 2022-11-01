613e9e75796c9ddbac170a0d3af59271c685d2d0
DericksonLossIT <dericksonlossit@gmail.com>
2022-11-01 19:21:26 -0300

    implements participant removal function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -31,14 +31,16 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado"),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
         style: "cancel",
       },
     ]);
-    console.log(`Você clicou para remover o participante ${name}`);
   }
 
   return (
